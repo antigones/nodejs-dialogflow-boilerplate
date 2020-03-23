a74ea1b922888849ff6b7bd928cbefad01433da6
antigones <antigones@users.noreply.github.com>
2020-03-23 23:27:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const {dialogflow, HtmlResponse} = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const app = dialogflow({debug: true});
 
 app.intent('Default Welcome Intent', (conv) => {
